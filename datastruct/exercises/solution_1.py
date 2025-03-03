3e8a63c01f67dc26d0955b6eb54d02af9aaa8915
JHONCE79 <jhonsteven.163@gmail.com>
2025-03-03 17:41:19 -0500

    incomplete exercise 2

--- datastruct/exercises/solution_1.py
+++ datastruct/exercises/solution_1.py
@@ -2,5 +2,13 @@ from datastruct.classes.lists import LinkedList
 
 
 def add_two_numbers(l1: LinkedList[int], l2: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    pass
+
+    list1 = l1.head
+    list2 = l2.head
+
+    sum_result = LinkedList[int]
+
+    while list1 is not None and list2 is not None:
+        if list1.value + list2.value >9:
+            pass
+        
--- datastruct/exercises/solution_2.py
+++ datastruct/exercises/solution_2.py
@@ -1,6 +1,30 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+
 from datastruct.classes.lists import LinkedList
 
-
 def swap_nodes_in_pairs(linked_list: LinkedList[int]) -> LinkedList[int]:
-    # Implement solution here
-    pass
+    if linked_list.head is None or linked_list.head.next is None:
+        return linked_list
+    
+    new_head = linked_list.head.next
+    prev = None
+    current = linked_list.head
+    
+    while current and current.next:
+        next_pair = current.next.next  
+        second = current.next          
+        
+        second.next = current
+        
+        if prev:
+            prev.next = second
+        
+        current.next = next_pair
+    
+    linked_list.head = new_head
+    
+    return linked_list
+
